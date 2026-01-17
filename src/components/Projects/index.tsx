38ebdb320d038b7e6ec071c598bccc27369fb89b
mateenshamsi <matinshamsi16@gmail.com>
2026-01-18 01:26:34 +0530

    Added React Lenis

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -15,7 +15,7 @@ export default function Projects() {
  
 
   return (
-    <div className="flex flex-col items-center justify-center max-w-[945px] mx-auto gap-6 md:gap-12 p-6 md:p-12">
+    <div  className="flex flex-col items-center justify-center max-w-[945px] mx-auto gap-6 md:gap-12 p-6 md:p-12">
       <div className="w-full">
         <BlurFade delay={BLUR_FADE_DELAY * 5}>
           <h1 className="text-xl sm:text-2xl md:text-4xl font-black mb-6 md:mb-8 text-foreground text-left">
