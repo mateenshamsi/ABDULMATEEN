a3a5bc6fd85f2ecf224e5aa8f847480d083ef9b3
mateenshamsi <matinshamsi16@gmail.com>
2026-01-19 13:42:14 +0530

    Changed favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,12 +27,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
+     
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+<link rel="shortcut icon" href="/favicon/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+<link rel="manifest" href="/favicon/site.webmanifest" />
       </head>
 
       <body className="relative  text-white overflow-hidden">
